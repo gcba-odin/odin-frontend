22d9328dec25b936dc38bcba1d84c86ac204f111
Cristian Garcia <informacion@cristiangarcia.co>
2016-05-16 17:41:18 -0200

    Organizing folders and files

---
+++ js/factories.js
@@ -0,0 +1,69 @@
+(function() {
+    var app = angular.module('store-factories', []);
+    app.factory("datasetF", function() {
+
+        var datasetRest = {
+            name: "",
+            description: "",
+            organization: "",
+            followers: [],
+            groups: [],
+            activity: [],
+            social: [],
+            licence: [],
+            resources: [],
+            tags: [],
+            aditionalInformation: []
+        }
+
+
+        var service = {
+            res: [],
+            initialDataset: function() {
+                service.res = datasetRest
+            },
+            setDataset: function(dataset) {
+                service.res = dataset;
+            },
+            getSocial: function() {
+                if (!!service.res.social) {
+                    return service.res.social;
+                } else {
+                    return false
+                }
+            },
+            getLicence: function() {
+                if (!!service.res.licence) {
+                    return service.res.licence;
+                } else {
+                    return false
+                }
+            },
+            getResources: function() {
+                if (service.res.resources.length) {
+                    return service.res.resources;
+                } else {
+                    return false
+                }
+            },
+            getTags: function() {
+                if (!!service.res.tags) {
+                    return service.res.tags;
+                } else {
+                    return false
+                }
+            },
+            getAditionalInformation: function() {
+                if (!!service.res.aditionalInformation) {
+                    return service.res.aditionalInformation;
+                } else {
+                    return false
+                }
+            }
+
+        }
+        return service;
+    })
+
+
+})();
