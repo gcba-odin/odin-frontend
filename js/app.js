a49640ce2d63174241482b8dc525b9091f0b9643
Cristian Garcia <informacion@cristiangarcia.co>
2016-05-16 17:45:55 -0200

    change language file inicio - home

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
     app.config(function($routeProvider) {
         $routeProvider
             .when("/", {
-                templateUrl: "inicio.html",
+                templateUrl: "home.html",
                 controller: controllerHome
             })
             .when("/dataset/:name", {
