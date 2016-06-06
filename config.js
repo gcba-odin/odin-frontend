a9800d50c8681f9b4b00afa700cf3b5575a23511
Cristian Garcia <informacion@cristiangarcia.co>
2016-06-06 15:24:49 -0200

    changing directives route

--- config.js
+++ config.js
@@ -4,8 +4,6 @@
     function run($rootScope, $location, $http) {
     	$rootScope.url='http://137.135.84.77:3000';
     	$rootScope.absUrl=$location.absUrl()
-        $rootScope.$on('$routeChangeSuccess', function (e, current, pre) {
-            $rootScope.actualUrl=current.$$route.originalPath;
-        });
+
     }
 })(); 
