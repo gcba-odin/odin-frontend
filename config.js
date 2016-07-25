a50d3126272c04007b870985e1fe8edd475364bd
Rita Zerrizuela <zeta@widcket.com>
2016-07-25 15:54:33 -0300

    Added favicons

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 (function() {
-    var app = angular.module('config-odin',[]);
+    var app = angular.module('config-odin', []);
     app.run(run);
+
     function run($rootScope, $location, $http) {
-    	$rootScope.url='http://137.135.84.77:3000';
-    	$rootScope.absUrl=$location.absUrl()
+        $rootScope.url = 'http://localhost:3000';
+        $rootScope.absUrl = $location.absUrl()
 
     }
-})(); 
+})();
