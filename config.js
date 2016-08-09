938e2c10dea0065130aa5806d46bf5acfa6cb9d3
Rita Zerrizuela <zeta@widcket.com>
2016-08-09 09:42:40 -0300

    Put config.js back in root

---
+++ config.js
@@ -0,0 +1,9 @@
+(function() {
+    var app = angular.module('config-odin', []);
+    app.run(run);
+
+    function run($rootScope, $location, $http) {
+        $rootScope.url = 'http://localhost:3000';
+        $rootScope.absUrl = $location.absUrl()
+    }
+})();
