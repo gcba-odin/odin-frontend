cca126e81a694b46ca289ce087ad3d56465e040e
Ubuntu <odintest@odintest.rxxi5m5vi5neznc3ovh2kwxnrg.cx.internal.cloudapp.net>
2016-08-09 13:21:22 +0000

    Reverted changes

--- config.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var app = angular.module('odin.config', []);
-    app.run(run);
-
-    function run($rootScope, $location, $http) {
-        $rootScope.url = 'http://localhost:3000';
-        $rootScope.absUrl = $location.absUrl()
-    }
-})();
