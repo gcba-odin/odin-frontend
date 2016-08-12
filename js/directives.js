d1f7bb999cb2eb75524ddb5e716f0e340d56df9a
maxi <maxiejbe92@gmail.com>
2016-08-12 01:19:07 -0300

    Submit with enter key in search controls

--- js/directives.js
+++ js/directives.js
@@ -31,6 +31,20 @@
     };
   });
 
+  app.directive('ngEnter', function() {
+      return function(scope, element, attrs) {
+          element.bind("keydown keypress", function(event) {
+              if(event.which === 13) {
+                  scope.$apply(function(){
+                      scope.$eval(attrs.ngEnter, {'event': event});
+                  });
+
+                  event.preventDefault();
+              }
+          });
+      };
+  });
+
   app.directive('svgImg', function(){
     return {
       restrict: 'A',
