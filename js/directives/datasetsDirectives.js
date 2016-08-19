4ad8d027237a3559b13b43656440d99eb7d3d413
aaccurso <accurso.alan@gmail.com>
2016-08-19 01:21:17 -0300

    Slugify dataset tags

--- js/directives/datasetsDirectives.js
+++ js/directives/datasetsDirectives.js
@@ -107,7 +107,7 @@
     });
 
 
-    app.directive("tagsResult", function(LocationSearchService) {
+    app.directive("tagsResult", function($filter, LocationSearchService) {
         return {
             restrict: "E",
             templateUrl: "directives/datasets/tags-results.html",
@@ -127,7 +127,8 @@
                     }, function() {
                         for (var i = 0; i < $scope.resultTags.data.length; i++) {
                             var tag = $scope.resultTags.data[i];
-                            tag.active = LocationSearchService.isActive(filterName, tag.name);
+                            tag.slug = $filter('slug')(tag.name);
+                            tag.active = LocationSearchService.isActive(filterName, tag.slug);
                             $scope.tags.push(tag);
                         }
                         $scope.lessThanLimit = $scope.resultTags.data.length < limit;
@@ -146,9 +147,9 @@
                 $scope.loadTags(0);
                 $scope.selectTag = function(tag) {
                     if(tag.active) {
-                        LocationSearchService.removeFilterValue(filterName, tag.name);
+                        LocationSearchService.removeFilterValue(filterName, tag.slug);
                     } else {
-                        LocationSearchService.addFilterValue(filterName, tag.name);
+                        LocationSearchService.addFilterValue(filterName, tag.slug);
                     }
                 };
                 $scope.removeAll = function() {
@@ -159,13 +160,14 @@
         };
     });
 
-    app.directive("formatsResult", function(LocationSearchService) {
+    app.directive("formatsResult", function($filter, LocationSearchService) {
         return {
             restrict: "E",
             templateUrl: "directives/datasets/formats-results.html",
             scope: {},
             controller: function($scope, rest) {
                 var filterName = 'files.type';
+                // var filterName = 'files.type.name';
                 $scope.limitFormats = 0;
                 $scope.filetypes = [];
                 $scope.resultFormats = [];
@@ -180,6 +182,7 @@
                         for (var i = 0; i < $scope.resultFormats.data.length; i++) {
                             var filetype = $scope.resultFormats.data[i];
                             filetype.active = LocationSearchService.isActive(filterName, filetype.id);
+                            filetype.slug = $filter('slug')(filetype.name);
                             $scope.filetypes.push(filetype);
                             $scope.loadFileTypeCount(filetype.id);
                         }
@@ -220,8 +223,10 @@
                 $scope.selectFiletype = function(filetype) {
                     if(filetype.active) {
                         LocationSearchService.removeFilterValue(filterName, filetype.id);
+                        // LocationSearchService.removeFilterValue(filterName, filetype.slug);
                     } else {
                         LocationSearchService.addFilterValue(filterName, filetype.id);
+                        // LocationSearchService.addFilterValue(filterName, filetype.slug);
                     }
                 };
                 $scope.removeAll = function() {
