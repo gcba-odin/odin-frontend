ac5165aa944a8cd81fa8491cc28ffca16a1ecf27
Santiago Martin Baudino <santyz.88@gmail.com>
2016-08-21 19:05:01 -0300

    interaccion con twitter

--- js/app.js
+++ js/app.js
@@ -12,12 +12,13 @@
         "bw.paging",
         "angularUtils.directives.dirDisqus",
         "720kb.socialshare",
-        "pdf"
+        "pdf",
+        "ngtweet"
     ]);
     app.config(function($routeProvider, $locationProvider) {
-        
+
         $locationProvider.html5Mode(true);
-        
+
         $routeProvider
                 .when("/", {
                     templateUrl: "views/home.html",
