803d83480fb43362d683fae33e6a547e8b098cbd
aaccurso <accurso.alan@gmail.com>
2016-08-22 01:00:43 -0300

    Cleans gulp tasks

--- gulp/serve.js
+++ gulp/serve.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp'),
-    historyApiFallback = require('connect-history-api-fallback');
+    connectLogger = require('connect-logger'),
+    connectHistoryApiFallback = require('connect-history-api-fallback');
 
 gulp.task('serve', ['build'], function() {
   return gulp.browserSync.init({
     server: {
       baseDir: './'
     },
-    middleware: [require("connect-logger")(), historyApiFallback()]
+    middleware: [connectLogger(), connectHistoryApiFallback()]
   });
 });
