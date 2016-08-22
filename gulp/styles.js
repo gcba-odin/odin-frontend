54eed3794d12c462779955da720a6173a8c23fbd
aaccurso <accurso.alan@gmail.com>
2016-08-22 00:47:39 -0300

    Adds css minification

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     sass = require('gulp-sass'),
+    cleanCSS = require('gulp-clean-css')
     sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', function () {
@@ -8,6 +9,8 @@ gulp.task('styles', function () {
   .pipe(plumber())
   .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(cleanCSS())
+    //TODO: add autoprefixer
   .pipe(sourcemaps.write())
   .pipe(gulp.dest('./dist'))
   .pipe(gulp.browserSync.stream());
