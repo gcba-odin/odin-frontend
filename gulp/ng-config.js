284b525356173ba7ffd1b92b7e11650807f194e2
aaccurso <accurso.alan@gmail.com>
2016-08-23 22:26:53 -0300

    Adds support for config.local.json

--- gulp/ng-config.js
+++ gulp/ng-config.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp'),
+    fs = require('fs'),
+    rename = require('gulp-rename'),
     ngConfig = require('gulp-ng-config');
 
 gulp.task('ng-config', function() {
-  return gulp.src('config.json')
+  var configFile = fs.existsSync('config.local.json') ?
+    'config.local.json' :
+    'config.json';
+  return gulp.src(configFile)
     .pipe(ngConfig('odin.config', {
       environment: process.env.NODE_ENV || 'local'
     }))
+    .pipe(rename('config.js'))
     .pipe(gulp.dest('js'));
 });
