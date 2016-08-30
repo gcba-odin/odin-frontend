7fb4d1cfa528136f88788e954c4b8ba70389ade5
aaccurso <accurso.alan@gmail.com>
2016-08-30 18:53:28 -0300

    Fixes sidebar full catalog not hiding on click and full width additional info table

--- gulp/base-url.js
+++ gulp/base-url.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
     config = require('../config.json'),
+    configLocal = require('../config.local.json'),
     template = require('gulp-template');
 
 gulp.task('base-url', function() {
   var env = process.env.NODE_ENV || 'local';
+  config = (configLocal ? configLocal : config);
   return gulp.src('index.html')
     .pipe(template({ baseUrl: config[env].BaseHTML5.url }))
     .pipe(gulp.dest('dist'));
