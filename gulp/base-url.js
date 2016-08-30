5e226ca47d75913ce178c04eebc38dc856016219
aaccurso <accurso.alan@gmail.com>
2016-08-30 18:58:13 -0300

    Fixes base-url gulp task

--- gulp/base-url.js
+++ gulp/base-url.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp'),
-    config = require('../config.json'),
-    configLocal = require('../config.local.json'),
+    fs = require('fs'),
     template = require('gulp-template');
 
 gulp.task('base-url', function() {
-  var env = process.env.NODE_ENV || 'local';
-  config = (configLocal ? configLocal : config);
+  var env = process.env.NODE_ENV || 'local',
+      configFile = fs.existsSync('config.local.json')
+        ? '../config.local.json'
+        : '../config.json',
+      config = require(configFile);
+
   return gulp.src('index.html')
     .pipe(template({ baseUrl: config[env].BaseHTML5.url }))
     .pipe(gulp.dest('dist'));
