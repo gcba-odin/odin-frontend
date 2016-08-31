251d60e11a8ecda20e4a83d864fc04eab2b3ffd1
Matias Tebele <mati.tebele@gmail.com>
2016-08-30 23:28:01 -0300

    Fixed coloring category in datasets.

--- js/controllers/categoryController.js
+++ js/controllers/categoryController.js
@@ -5,7 +5,6 @@ app.factory('model', function($resource) {
 });
 
 function CategoryListController($scope, $location, rest, $rootScope, $routeParams, $httpParamSerializer) {
-    $scope.activeCategories = [];
     $scope.activeCategory = $routeParams['categories.name'];
     $scope.url_api = $rootScope.url;
     $scope.activeCategory = $.isArray($scope.activeCategory) ? $scope.activeCategory[0] : $routeParams['categories.name'];
