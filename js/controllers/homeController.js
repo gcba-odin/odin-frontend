07d8d6b1754e83dacca5a0c0dead4abdd7fec466
Matias Tebele <mati.tebele@gmail.com>
2016-09-02 17:29:57 -0300

    Added dataset counter in the home.

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,6 +1,19 @@
-function controllerHome($scope, $location, $sce, $filter, $rootScope) {
+function controllerHome($scope, $location, $sce, $filter, $rootScope, rest, DatasetListService) {
     $rootScope.header = "Odin";
     $rootScope.isDatasetView = false;
+    $scope.type = "datasets";
+    $scope.params = {
+        limit: 20
+    };
+
+    rest().get({
+        type: $scope.type
+    }, function(result) {
+        $scope.params.limit = result.meta.limit * result.meta.pages;
+        DatasetListService.getDatasets($scope.params, function(datasets) {
+            $scope.countDatasets = datasets.length;
+        });
+    });
 
     $scope.getHtml = function(html) {
         return $sce.trustAsHtml(html);
