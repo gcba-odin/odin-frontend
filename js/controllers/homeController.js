de4b26e585b8d965745580301c58837dd697faeb
maxi <maxiejbe92@gmail.com>
2016-09-07 16:25:47 -0300

    Fixes home datasets count

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,18 +1,9 @@
 function controllerHome($scope, $location, $sce, $filter, $rootScope, rest, DatasetListService) {
     $rootScope.header = "Odin";
     $rootScope.isDatasetView = false;
-    $scope.type = "datasets";
-    $scope.params = {
-        limit: 20
-    };
-
-    rest().get({
-        type: $scope.type
-    }, function(result) {
-        $scope.params.limit = result.meta.limit * result.meta.pages;
-        DatasetListService.getDatasets($scope.params, function(datasets) {
-            $scope.countDatasets = datasets.length;
-        });
+    
+    DatasetListService.getDatasets($scope.params, function(datasets) {
+        $scope.countDatasets = datasets.length;
     });
 
     $scope.getHtml = function(html) {
