1bf1e6af2537930d0f7320d7c07701be2ea20636
elstr <eleonora.lester@gmail.com>
2017-01-06 14:37:58 -0300

    add authorization on post contact form

--- js/controllers/contactController.js
+++ js/controllers/contactController.js
@@ -1,4 +1,4 @@
-function contactController($scope, $http, vcRecaptchaService, EnvironmentConfig) {
+function contactController($scope, $http, vcRecaptchaService, EnvironmentConfig,  $rootScope) {
     recaptchaId = null;
     $scope.setRecaptchaId = function (widgetId) {
         console.log(widgetId);
@@ -6,20 +6,26 @@ function contactController($scope, $http, vcRecaptchaService, EnvironmentConfig)
     };
 
     $scope.submit = function () {
+      debugger;
         if (!vcRecaptchaService.getResponse(recaptchaId)) {
             $scope.od_captcha = null;
             vcRecaptchaService.reload(recaptchaId);
             alert('Por favor, completa el captcha.');
         } else {
+          var token=$rootScope.globals.currentUser.token;
           var formData = $("#contactForm").serialize();
           $.ajax({
+            headers: {
+                'Authorization': 'Bearer ' + token
+            },
             type: 'POST',
             url:  EnvironmentConfig.api + '/email/send',
             data: formData
           }).done(function(response) {
             showMsg(true);
           }).fail(function(data) {
-            showMsg(false);
+            // showMsg(data);
+            console.log(data);
           });
         }
     }
@@ -28,7 +34,7 @@ function contactController($scope, $http, vcRecaptchaService, EnvironmentConfig)
       if (result) {
         alert('El mensaje ha sido enviado correctamente');
       } else {
-        alert('El mensaje no ha sido enviado, intente nuevamente más tarde');
+        // alert('El mensaje no ha sido enviado, intente nuevamente más tarde');
       }
       document.location.href="/";
     }
