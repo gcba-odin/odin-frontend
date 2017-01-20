9e69607934ae3618e2a4ea56879316cdf3f7225c
elstr <eleonora.lester@gmail.com>
2017-01-20 09:18:26 -0300

    changes filters

---
+++ js/controllers/datasetFilters/filetypesController.js
@@ -0,0 +1,88 @@
+angular.module('odin.controllers')
+.controller('FiletypesController', FiletypesController);
+
+function FiletypesController($filter, $routeParams, $rootScope, $scope, rest, LocationSearchService, DatasetListService) {
+    var filterName = 'files.type';
+    const limit = 5;
+    $scope.limitFormats = 0;
+    $scope.filetypes = [];
+    $scope.resultFormats = [];
+    $scope.lessThanLimit;
+    $scope.fileTypesCount = {};
+
+    $scope.collapsed = true;
+    $scope.toggleCollapse = function() {
+        $scope.collapsed = !$scope.collapsed;
+    };
+
+    $scope.loadFormats = function(skip) {
+        $scope.limitFormats += skip;
+        $scope.resultFormats = rest().get({
+            type: "filetypes",
+            params: "orderBy=name&sort=ASC&limit=5&skip=" + $scope.limitFormats
+        }, function() {
+            for (var i = 0; i < $scope.resultFormats.data.length; i++) {
+                var filetype = $scope.resultFormats.data[i];
+                filetype.active = LocationSearchService.isActive(filterName, filetype.id);
+                filetype.slug = $filter('slug')(filetype.name);
+                $scope.filetypes.push(filetype);
+                $scope.loadFileTypeCount(filetype.id);
+            }
+            $scope.lessThanLimit = $scope.resultFormats.data.length < Math.max(skip, limit);
+        });
+        $scope.datasetCount = {};
+    };
+
+    //This won't scale. TODO: Change to /count
+    $scope.loadFileTypeCount = function(fileTypeId){
+        $scope.fileTypesCount[fileTypeId] = 0;
+        $scope.params = {
+            condition: 'AND',
+            include: ['files', 'tags', 'categories'].join(),
+            'files.type': fileTypeId,
+            'categories.slug': $routeParams['categories.slug'],
+        };
+        DatasetListService.getDatasetsCount($scope.params, function(result) {
+            $scope.fileTypesCount[fileTypeId] = result.data.count;
+        });
+    };
+
+    $scope.showLess = function(limit) {
+        var countFormats = $scope.filetypes.length;
+        var minCount = Math.min(countFormats, limit);
+        $scope.filetypes.splice(minCount, countFormats - minCount);
+        $scope.limitFormats = 0;
+        $scope.lessThanLimit = false;
+    };
+
+    $scope.loadFormats(0);
+    $scope.selectFiletype = function(filetype) {
+        $rootScope.showFiltersMenu = false;
+        $rootScope.showBackdrop = false;
+        if(filetype.active) {
+            LocationSearchService.removeFilterValue(filterName, filetype.id);
+        } else {
+            LocationSearchService.addFilterValue(filterName, filetype.id);
+        }
+    };
+    $scope.removeAll = function() {
+        LocationSearchService.deleteFilter(filterName);
+    };
+
+    var currentColor;
+    var category = rest().get({
+      type: 'categories',
+      params: 'slug='+$routeParams['categories.slug']+"&match=exact"
+    }, function(resp) {
+      if (resp.data[0]) {
+        $scope.currentCategory = resp.data[0];
+        if ($scope.currentCategory.color !== null && $scope.currentCategory.color !== undefined) {
+            $scope.currentColor = $scope.currentCategory.color ;
+            sessionStorage.setItem('currentColor', $scope.currentColor);
+        }
+      }else{
+        $scope.currentColor = sessionStorage.getItem('currentColor');
+      }
+    });
+
+}
---
+++ js/controllers/datasetFilters/tagsController.js
@@ -0,0 +1,71 @@
+angular.module('odin.controllers')
+.controller('TagsController', TagsController);
+
+function TagsController($rootScope, $scope, $filter, rest, LocationSearchService, $routeParams) {
+    var filterName = 'tags.slug';
+    const limit = 5;
+    $scope.limitTags = 0;
+    $scope.tags = [];
+    $scope.resultTags = [];
+    $scope.lessThanLimit;
+
+    $scope.collapsed = true;
+    $scope.toggleCollapse = function() {
+        $scope.collapsed = !$scope.collapsed;
+    };
+    $scope.loadTags = function(skip) {
+        $scope.limitTags += skip;
+        $scope.resultTags = rest().get({
+            type: "tags",
+            params: "orderBy=name&sort=ASC&limit=5&skip=" + $scope.limitTags
+        }, function() {
+            for (var i = 0; i < $scope.resultTags.data.length; i++) {
+                var tag = $scope.resultTags.data[i];
+                tag.slug = $filter('slug')(tag.name);
+                tag.active = LocationSearchService.isActive(filterName, tag.slug);
+                $scope.tags.push(tag);
+            }
+            $scope.lessThanLimit = $scope.resultTags.data.length < Math.max(skip, limit);
+        });
+
+    };
+
+    $scope.showLess = function(limit) {
+        var countTags = $scope.tags.length;
+        var minCount = Math.min(countTags, limit);
+        $scope.tags.splice(minCount, countTags - minCount);
+        $scope.limitTags = 0;
+        $scope.lessThanLimit = false;
+    };
+
+    $scope.loadTags(0);
+    $scope.selectTag = function(tag) {
+        $rootScope.showFiltersMenu = false;
+        $rootScope.showBackdrop = false;
+        if(tag.active) {
+            LocationSearchService.removeFilterValue(filterName, tag.slug);
+        } else {
+            LocationSearchService.addFilterValue(filterName, tag.slug);
+        }
+    };
+    $scope.removeAll = function() {
+        LocationSearchService.deleteFilter(filterName);
+    };
+
+    var currentColor;
+    var category = rest().get({
+      type: 'categories',
+      params: 'slug='+$routeParams['categories.slug']+"&match=exact"
+    }, function(resp) {
+      if (resp.data[0]) {
+        $scope.currentCategory = resp.data[0];
+        if ($scope.currentCategory.color !== null && $scope.currentCategory.color !== undefined) {
+            $scope.currentColor = $scope.currentCategory.color ;
+            sessionStorage.setItem('currentColor', $scope.currentColor);
+        }
+      }else{
+        $scope.currentColor = sessionStorage.getItem('currentColor');
+      }
+    });
+
+}
