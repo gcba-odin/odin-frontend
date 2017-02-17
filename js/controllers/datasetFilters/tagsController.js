f5094916ec7077c8e85eb364e47611829b847c26
Eleonora Lester <elester@MacBook-Air-de-Eleonora.local>
2017-02-17 16:53:41 -0300

    clear unused code

--- js/controllers/datasetFilters/tagsController.js
+++ js/controllers/datasetFilters/tagsController.js
@@ -20,26 +20,7 @@ function TagsController($rootScope, $scope, $filter, rest, LocationSearchService
     if (!$.isArray($scope.selectedTags)) {
       $scope.selectedTags = [];
     }
-    // $scope.loadTags = function(skip) {
-    //     $scope.limitTags += skip;
-    //     $scope.resultTags = rest().get({
-    //         type: "tags",
-    //         params: "orderBy=name&sort=ASC&limit=5&skip=" + $scope.limitTags
-    //     }, function() {
-    //         for (var i = 0; i < $scope.resultTags.data.length; i++) {
-    //             var tag = $scope.resultTags.data[i];
-    //             tag.slug = $filter('slug')(tag.name);
-    //             tag.active = LocationSearchService.isActive(filterName, tag.slug);
-    //             $scope.tags.push(tag);
-    //             $scope.tagNames.push(tag.name);
-    //         }
-    //
-    //         if ($scope.tags.filter(tag=>tag.active)[0]!==undefined) {
-    //           $scope.collapsed=false;
-    //         }
-    //         $scope.lessThanLimit = $scope.resultTags.data.length < Math.max(skip, limit);
-    //     });
-    // };
+
     $scope.loadTags = function() {
         $scope.resultTags = rest().get({
             type: "tags",
@@ -52,6 +33,7 @@ function TagsController($rootScope, $scope, $filter, rest, LocationSearchService
                 $scope.tags.push(tag);
                 $scope.tagNames.push(tag.name);
             }
+
             tagsAutocomplete = JSON.parse(sessionStorage.getItem('tagsAutocomplete'));
             if (tagsAutocomplete) {
               $scope.tagNames=tagsAutocomplete;
@@ -61,23 +43,15 @@ function TagsController($rootScope, $scope, $filter, rest, LocationSearchService
             if ($scope.tags.filter(tag=>tag.active)[0]!==undefined) {
               $scope.collapsed=false;
             }
-            // $scope.lessThanLimit = $scope.resultTags.data.length < Math.max(skip, limit);
+
         });
     };
 
-    // $scope.showLess = function(limit) {
-    //     var countTags = $scope.tags.length;
-    //     var minCount = Math.min(countTags, limit);
-    //     $scope.tags.splice(minCount, countTags - minCount);
-    //     $scope.limitTags = 0;
-    //     $scope.lessThanLimit = false;
-    // };
 
     $scope.loadTags(0);
     $scope.selectTag = function(tag) {
         $rootScope.showFiltersMenu = false;
         $rootScope.showBackdrop = false;
-        debugger;
         if(tag.active) {
             $scope.selectedTags.splice($scope.selectedTags.indexOf(tag.name),1);
             tagsAutocomplete.push(tag)
