9c6056cade5a1e043540fe136986277f96e9dd97
Maximiliano Angeletti <max.angletti@gmail.com>
2017-03-13 11:46:20 -0300

    propose dataset form

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -11,19 +11,55 @@ function controllerHome($scope, $location, $sce, $filter, $rootScope, rest, Data
     $rootScope.showLoadingLatest = true;
     $rootScope.showLoadingStarred = true;
 
-    $rootScope.countQuery ++;
-    DatasetListService.getDatasetsCount($scope.params, function(result) {
+    $rootScope.countQuery++;
+    DatasetListService.getDatasetsCount($scope.params, function (result) {
         $rootScope.countDatasets = result.data.count;
-        $rootScope.countQuery --;
-        if($rootScope.countQuery == 0) { usSpinnerService.stop('spinner'); }
+        $rootScope.countQuery--;
+        if ($rootScope.countQuery == 0) {
+            usSpinnerService.stop('spinner');
+        }
     });
 
-    $scope.getHtml = function(html) {
+    $scope.getHtml = function (html) {
         return $sce.trustAsHtml(html);
     };
 
-    $scope.goToUrl = function(url) {
+    $scope.goToUrl = function (url) {
         $filter('slug')(this.item.name);
         window.location = "/dataset/" + $filter('slug')(this.item.id);
     };
 }
+
+function ProposeController($scope, $rootScope, vcRecaptchaService) {
+    $rootScope.isHome = false;
+
+    $scope.activeCategory = [];
+    $scope.categories = JSON.parse(sessionStorage.getItem('categories')) || [];
+    $rootScope.dataCategories = $scope.categories;
+
+    recaptchaId = null;
+    $scope.setRecaptchaId = function (widgetId) {
+        recaptchaId = widgetId;
+    };
+
+    $scope.send = function () {
+        if($scope.activeCategory.length == 0) {
+            alert('Por favor, seleccioná al menos una categoría.');
+        } else if (!vcRecaptchaService.getResponse(recaptchaId)) {
+            $scope.od_captcha = null;
+            vcRecaptchaService.reload(recaptchaId);
+            alert('Por favor, completa el captcha.');
+        } else {
+            alert('Al Gobierno Abierto lo construimos todos, ¡Gracias por tu sugerencia!');
+        }
+    };
+
+    $scope.toogleActive = function (slug) {
+        if ($scope.activeCategory.indexOf(slug) === -1) {
+            $scope.activeCategory.push(slug);
+        } else {
+            $scope.activeCategory.splice($scope.activeCategory.indexOf(slug), 1);
+        }
+    };
+}
+;
