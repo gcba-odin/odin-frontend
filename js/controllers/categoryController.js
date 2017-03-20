7a5f162548534d4c40a03d0dc5779c2507f3aebe
Eleonora Lester <elester@MacBook-Air-de-Eleonora.local>
2017-03-20 14:54:10 -0300

    categories arrangement

--- js/controllers/categoryController.js
+++ js/controllers/categoryController.js
@@ -4,10 +4,10 @@ app.factory('model', function ($resource) {
     return $resource();
 });
 
-function CategoryListController($scope, $location, rest, $rootScope, $routeParams, $httpParamSerializer, $log, usSpinnerService) {
+function CategoryListController($scope, $location, rest, $rootScope, $routeParams, $httpParamSerializer, $log, $timeout, usSpinnerService) {
     usSpinnerService.spin('spinner');
     $rootScope.countQuery = $rootScope.countQuery + 4;
-    
+
     var cache = {
         categories: {
             cache: false,
@@ -45,6 +45,7 @@ function CategoryListController($scope, $location, rest, $rootScope, $routeParam
     $scope.porcentual = {};
     $scope.totalStatistics = 0;
     $scope.letterLimit = 4;
+
     $scope.hideCategoriesSidebar = function () {
         $rootScope.showBackdrop = false;
         $rootScope.showCategoriesSidebar = false;
@@ -87,7 +88,7 @@ function CategoryListController($scope, $location, rest, $rootScope, $routeParam
             }
         }
     });
-    
+
     //Tags cache
     $scope.$watch(function () {
         if (!!sessionStorage.getItem('tags')) {
@@ -220,7 +221,7 @@ function CategoryListController($scope, $location, rest, $rootScope, $routeParam
         });
     }
     ;
-    
+
     //Tags cache
     if (!!sessionStorage.getItem('tags')) {
         $rootScope.dataTags = JSON.parse(sessionStorage.getItem('tags'));
@@ -256,7 +257,7 @@ function CategoryListController($scope, $location, rest, $rootScope, $routeParam
         });
     }
     ;
-    
+
     //Organizations cache
     if (!!sessionStorage.getItem('organizations')) {
         $rootScope.dataOrgs = JSON.parse(sessionStorage.getItem('organizations'));
@@ -292,6 +293,12 @@ function CategoryListController($scope, $location, rest, $rootScope, $routeParam
         });
     }
     ;
+
+    $timeout(function(){
+      usSpinnerService.spin('spinner');
+      arrangeCategories();
+      usSpinnerService.stop('spinner');
+    });
 }
 
 
@@ -309,4 +316,94 @@ function clearFilters(){
   sessionStorage.removeItem('selectedTags');
   sessionStorage.removeItem('selectedOrgs');
   sessionStorage.removeItem('selectedFormats');
+}
+
+function arrangeCategories(){
+    var container = $('#categoriesgrid-container').width();
+    if (container > 0) {
+      var items = $('.arrange-item');
+      var cant = items.length;
+
+      // Obtains max categs that fit the container
+      var max = Math.trunc(container/items.outerWidth(true));
+      var limit = Math.trunc(max/2);
+      // Only if max < categ quantity => rearrange
+      if (cant>max) {
+        var combinations = [];
+        // 1) Obtains all the combinations
+        for (var i = max; i >= limit; i--) {
+          var remainder;
+          var combination = [];
+          var first = true;
+          do {
+            if (first) {
+              remainder = cant - i;
+              first=false;
+            } else {
+              remainder = remainder - i;
+            }
+              combination.push(i)
+            if (remainder > 0 && remainder < i) {
+                combination.push(remainder)
+            }
+          } while (remainder != 0 && remainder >= i);
+
+          // 2) Save the combination if last item != 1
+          if (combination[combination.length-1]!=1) {
+            combinations.push(combination);
+          }
+        }
+
+        var remainderIndex=[];
+
+        // 3) Calculate remainder and save remainder/index
+        for (var i = 0; i < combinations.length; i++) {
+          combinations[i].reverse(); // magic
+          var total;
+          for (var x = 0; x < combinations[i].length; x++) {
+            if (x == 0) {
+              total = combinations[i][0];
+            } else {
+              total -= combinations[i][x];
+            }
+          }
+          var result = [];
+          result.push(total);        // Save Remainder
+          result.push(i);            // Save Index
+          remainderIndex.push(result)// Save Combination
+        }
+
+        // 4) Take highest positive value and index
+        var max, maxAcum, index;
+        max = remainderIndex[0][0];
+        index = remainderIndex[0][1];
+        for (var i = 1; i < remainderIndex.length; i++) {
+          if (remainderIndex[i][0] == 0 || remainderIndex[i][0] > max ) {
+            max = remainderIndex[i][0];
+            index = i;
+          }
+        }
+
+        // 5) Arrange categories
+        var selected = combinations[index].reverse()
+        var item;
+        var index=[];
+        var $blocker = $('<div />');
+        var $lineBreak = $('<br/>')
+
+        for (var i = 0; i < selected.length-1; i++) {
+          var indexItem =(selected[i]);
+          if (item === undefined) {
+            item = indexItem - 1;
+            $blocker.appendTo(items[item].parentElement)
+          } else {
+            item = item + indexItem;
+          }
+
+          index.push(items[item]);
+          $lineBreak.appendTo(items[item].parentElement)
+
+        }
+      }
+    }
 }
--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -30,7 +30,7 @@ function controllerHome($scope, $location, $sce, $filter, $rootScope, rest, Data
     };
 }
 
-function ProposeController($scope, $rootScope, vcRecaptchaService, Alertify) {
+function ProposeController($scope, $rootScope, $timeout, vcRecaptchaService, Alertify) {
     $rootScope.isHome = false;
 
     $scope.activeCategory = [];
@@ -61,8 +61,107 @@ function ProposeController($scope, $rootScope, vcRecaptchaService, Alertify) {
             $scope.activeCategory.splice($scope.activeCategory.indexOf(slug), 1);
         }
     };
+
+    $timeout(function(){
+      // usSpinnerService.spin('spinner');
+      arrangeCategories();
+      // usSpinnerService.stop('spinner');
+    });
 }
 ;
+
+function arrangeCategories(){
+    var container = $('#categoriesgrid-container').width();
+    if (container > 0) {
+
+      var items = $('.arrange-item');
+      var cant = items.length;
+
+      // Obtains max categs that fit the container
+      var max = Math.trunc(container/items.outerWidth(true));
+
+      var limit = Math.trunc(max/2);
+      // Only if max < categ quantity => rearrange
+      if (cant>max) {
+        var combinations = [];
+        // 1) Obtains all the combinations
+        for (var i = max; i >= limit; i--) {
+          var remainder;
+          var combination = [];
+          var first = true;
+          do {
+            if (first) {
+              remainder = cant - i;
+              first=false;
+            } else {
+              remainder = remainder - i;
+            }
+              combination.push(i)
+            if (remainder > 0 && remainder < i) {
+                combination.push(remainder)
+            }
+          } while (remainder != 0 && remainder >= i);
+
+          // 2) Save the combination if last item != 1
+          if (combination[combination.length-1]!=1) {
+            combinations.push(combination);
+          }
+        }
+
+        var remainderIndex=[];
+
+        // 3) Calculate remainder and save remainder/index
+        for (var i = 0; i < combinations.length; i++) {
+          combinations[i].reverse(); // magic
+          var total;
+          for (var x = 0; x < combinations[i].length; x++) {
+            if (x == 0) {
+              total = combinations[i][0];
+            } else {
+              total -= combinations[i][x];
+            }
+          }
+          var result = [];
+          result.push(total);        // Save Remainder
+          result.push(i);            // Save Index
+          remainderIndex.push(result)   // Save Combination
+        }
+
+        // 4) Take highest positive value and index
+        var max, maxAcum, index;
+        max = remainderIndex[0][0];
+        index = remainderIndex[0][1];
+        for (var i = 1; i < remainderIndex.length; i++) {
+          if (remainderIndex[i][0] == 0 || remainderIndex[i][0] > max ) {
+            max = remainderIndex[i][0];
+            index = i;
+          }
+        }
+
+        // 5) Arrange categories
+        var selected = combinations[index].reverse()
+        var item;
+        var index=[];
+        var $blocker = $('<div />');
+        var $lineBreak = $('<br/>')
+
+        for (var i = 0; i < selected.length-1; i++) {
+          var indexItem =(selected[i]);
+          if (item === undefined) {
+            item = indexItem - 1;
+            $blocker.appendTo(items[item].parentElement)
+          } else {
+            item = item + indexItem;
+          }
+
+          index.push(items[item]);
+          $lineBreak.appendTo(items[item].parentElement)
+
+        }
+      }
+    }
+}
+
 
 function OdinGirlController($scope) {
     $scope.hideGirl = false;
