b50a2f7c04f623cf5d8af6a6754c6ea004f16d8f
Eleonora Lester <elester@MacBook-Air-de-Eleonora.local>
2017-03-22 08:52:19 -0300

    fix spinner

--- js/controllers/categoryController.js
+++ js/controllers/categoryController.js
@@ -4,7 +4,7 @@ app.factory('model', function ($resource) {
     return $resource();
 });
 
-function CategoryListController($scope, $location, rest, $rootScope, $routeParams, $httpParamSerializer, $log, $timeout, usSpinnerService) {
+function CategoryListController($scope, $location, rest, $rootScope, $routeParams, $httpParamSerializer, $log, usSpinnerService) {
     usSpinnerService.spin('spinner');
     $rootScope.countQuery = $rootScope.countQuery + 4;
 
@@ -294,11 +294,7 @@ function CategoryListController($scope, $location, rest, $rootScope, $routeParam
     }
     ;
 
-    $timeout(function(){
-      usSpinnerService.spin('spinner');
-      arrangeCategories();
-      usSpinnerService.stop('spinner');
-    });
+
 }
 
 
@@ -316,94 +312,4 @@ function clearFilters(){
   sessionStorage.removeItem('selectedTags');
   sessionStorage.removeItem('selectedOrgs');
   sessionStorage.removeItem('selectedFormats');
-}
-
-function arrangeCategories(){
-    var container = $('#categoriesgrid-container').width();
-    if (container > 0) {
-      var items = $('.arrange-item');
-      var cant = items.length;
-
-      // Obtains max categs that fit the container
-      var max = Math.trunc(container/items.outerWidth(true));
-      var limit = Math.trunc(max/2);
-      // Only if max < categ quantity => rearrange
-      if (cant>max) {
-        var combinations = [];
-        // 1) Obtains all the combinations
-        for (var i = max; i >= limit; i--) {
-          var remainder;
-          var combination = [];
-          var first = true;
-          do {
-            if (first) {
-              remainder = cant - i;
-              first=false;
-            } else {
-              remainder = remainder - i;
-            }
-              combination.push(i)
-            if (remainder > 0 && remainder < i) {
-                combination.push(remainder)
-            }
-          } while (remainder != 0 && remainder >= i);
-
-          // 2) Save the combination if last item != 1
-          if (combination[combination.length-1]!=1) {
-            combinations.push(combination);
-          }
-        }
-
-        var remainderIndex=[];
-
-        // 3) Calculate remainder and save remainder/index
-        for (var i = 0; i < combinations.length; i++) {
-          combinations[i].reverse(); // magic
-          var total;
-          for (var x = 0; x < combinations[i].length; x++) {
-            if (x == 0) {
-              total = combinations[i][0];
-            } else {
-              total -= combinations[i][x];
-            }
-          }
-          var result = [];
-          result.push(total);        // Save Remainder
-          result.push(i);            // Save Index
-          remainderIndex.push(result)// Save Combination
-        }
-
-        // 4) Take highest positive value and index
-        var max, maxAcum, index;
-        max = remainderIndex[0][0];
-        index = remainderIndex[0][1];
-        for (var i = 1; i < remainderIndex.length; i++) {
-          if (remainderIndex[i][0] == 0 || remainderIndex[i][0] > max ) {
-            max = remainderIndex[i][0];
-            index = i;
-          }
-        }
-
-        // 5) Arrange categories
-        var selected = combinations[index].reverse()
-        var item;
-        var index=[];
-        var $blocker = $('<div />');
-        var $lineBreak = $('<br/>')
-
-        for (var i = 0; i < selected.length-1; i++) {
-          var indexItem =(selected[i]);
-          if (item === undefined) {
-            item = indexItem - 1;
-            $blocker.appendTo(items[item].parentElement)
-          } else {
-            item = item + indexItem;
-          }
-
-          index.push(items[item]);
-          $lineBreak.appendTo(items[item].parentElement)
-
-        }
-      }
-    }
 }
