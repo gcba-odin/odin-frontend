5cbf8edc4f947ea6f98a2d73ae4c18c08c55ce01
Eleonora Lester <elester@MacBook-Air-de-Eleonora.local>
2017-04-25 10:52:44 -0300

    refactor dataset statistics

--- js/controllers/datasetPopularController.js
+++ js/controllers/datasetPopularController.js
@@ -1,43 +1,33 @@
 function DatasetPopularController($scope, $location, rest, $rootScope, $sce, usSpinnerService) {
     usSpinnerService.spin('spinner');
-    $rootScope.countQuery ++;    
+    $rootScope.countQuery ++;
     $scope.showPopularCategories = false;
-    $scope.statistics = {};
-    $scope.porcentual = {};
     $scope.totalStatistics = 0;
     $scope.letterLimit = 4;
     $scope.hideCategoriesSidebar = function () {
         $rootScope.showBackdrop = false;
         $rootScope.showCategoriesSidebar = false;
     };
-    
-    $scope.popularCategories = angular.copy($scope.popCats);
-    
-    var loadPorcentual = function (categories) {
-        for (var i = 0; i < categories.length; i++) {
-            categories[i]['porcentaje'] = 0;
-        }
-        categories.forEach(function (element) {
-            $scope.statistics[element.id] = 0;
-            $scope.porcentual[element.id] = 0;
-        });
 
+    $scope.popularCategories = angular.copy($scope.popCats);
+
+    var loadPorcentual = function (categories) {
         rest().statistics({
             type: "datasets",
             params: "groupBy=category&action=download"
         }, function (statistics) {
+
+        // TODO: recieve total from API
             for (element in statistics.data) {
-                var cat = statistics.data[element];
-                $scope.statistics[element] = cat.count.GET;
-                $scope.totalStatistics += cat.count.GET;
+                $scope.totalStatistics += statistics.data[element].count.GET;
             }
-            for (element in statistics.data) {
-                for (var i = 0; i < categories.length; i++) {
-                    if (categories[i].id == element) {
-                        categories[i]['porcentaje'] = $scope.statistics[element] * 100 / $scope.totalStatistics;
-                    }
-                }
+
+            for (var i = 0; i < categories.length; i++) {
+              if (!!statistics.data[categories[i].id]) {
+                categories[i]['porcentaje'] = statistics.data[categories[i].id].count.GET * 100 / $scope.totalStatistics;
+              }
             }
+
             categories.sort(function (a, b) {
                 return b['porcentaje'] - a['porcentaje'];
             });
@@ -51,6 +41,6 @@ function DatasetPopularController($scope, $location, rest, $rootScope, $sce, usS
             if($rootScope.countQuery == 0) { usSpinnerService.stop('spinner'); }
         });
     };
-    
+
     loadPorcentual($scope.popularCategories);
 }
